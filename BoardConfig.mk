abb20502a4065760f1150da91ee27c52a7813b3d
andrew.boren <andrew.boren@gmail.com>
2011-08-01 18:53:06 -0700

    first commit

---
+++ BoardConfig.mk
@@ -0,0 +1,108 @@
+USE_CAMERA_STUB := true
+
+# inherit from the proprietary version
+-include vendor/htc/shooter/BoardConfigVendor.mk
+
+TARGET_NO_BOOTLOADER := true
+
+TARGET_BOARD_PLATFORM := msm8660
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_SMP := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+TARGET_BOOTLOADER_BOARD_NAME := shooter
+TARGET_HAVE_HDMI_OUT := true
+TARGET_USES_OVERLAY := true
+TARGET_NO_RADIOIMAGE := true
+TARGET_HAVE_TSLIB := false
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+
+# Wifi related defines
+BOARD_WPA_SUPPLICANT_DRIVER := WEXT
+WPA_SUPPLICANT_VERSION      := VER_0_6_X
+BOARD_WLAN_DEVICE           := bcm4329
+WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/bcm4329.ko"
+WIFI_DRIVER_FW_STA_PATH     := "/system/etc/firmware/fw_bcm4329.bin"
+WIFI_DRIVER_FW_AP_PATH      := "/system/etc/firmware/fw_bcm4329_apsta.bin"
+WIFI_DRIVER_MODULE_ARG      := "firmware_path=/system/etc/firmware/fw_bcm4329.bin nvram_path=/proc/calibration"
+WIFI_DRIVER_MODULE_NAME     := "bcm4329"
+
+# Audio 
+BOARD_USES_GENERIC_AUDIO := false
+BOARD_PREBUILT_LIBAUDIO := false
+TARGET_PROVIDES_LIBAUDIO := true
+
+#Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+
+# Define egl.cfg location
+BOARD_EGL_CFG := device/htc/shooter/egl.cfg
+
+BOARD_USES_QCOM_LIBS := true
+BOARD_USES_QCOM_LIBRPC := true
+#BOARD_USES_QCOM_GPS := true
+BOARD_USE_QCOM_PMEM := true
+BOARD_CAMERA_USE_GETBUFFERINFO := true
+
+
+#BOARD_OVERLAY_FORMAT_YCbCr_420_SP := true
+BOARD_USES_ADRENO_200 := true
+#BOARD_NO_RGBX_8888 := true
+
+BOARD_HAVE_FM_RADIO := true
+BOARD_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
+BOARD_USE_BROADCOM_FM_VOLUME_HACK := true
+
+TARGET_BOOTLOADER_BOARD_NAME := shooter
+
+BOARD_USE_NEW_LIBRIL_HTC := true
+TARGET_PROVIDES_LIBRIL := vendor/htc/shooter/proprietary/libril.so
+
+BOARD_KERNEL_CMDLINE := no_console_suspend=1
+BOARD_KERNEL_BASE := 0x40400000
+BOARD_PAGE_SIZE := 2048
+
+TARGET_USE_SCORPION_BIONIC_OPTIMIZATION := true
+
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := shooter
+BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50000
+
+TARGET_HAVE_HDMI_OUT := true
+TARGET_HARDWARE_3D := true
+
+# Define Prebuilt kernel locations
+TARGET_PREBUILT_KERNEL := device/htc/shooter/kernel
+
+# cat /proc/emmc
+# dev:        size     erasesize name
+# mmcblk0p31: 000ffa00 00000200 "misc"
+# mmcblk0p21: 00fffc00 00000200 "recovery"
+# mmcblk0p20: 01000000 00000200 "boot"
+# mmcblk0p22: 31fffc00 00000200 "system"
+# mmcblk0p24: 077fde00 00000200 "cache"
+# mmcblk0p23: 4aabc400 00000200 "userdata"
+
+
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16776192
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 838860800
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 20044333056
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+TARGET_RELEASETOOLS_EXTENSIONS := device/htc/common
+
+BOARD_SDCARD_DEVICE_PRIMARY := /dev/block/mmcblk1p1
+BOARD_SDCARD_DEVICE_SECONDARY := /dev/block/mmcblk1
+BOARD_SDEXT_DEVICE := /dev/block/mmcblk1p2
+BOARD_USES_MMCUTILS := true
+BOARD_HAS_NO_MISC_PARTITION := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_CUSTOM_GRAPHICS:= ../../../device/htc/shooter/graphics.c
+BOARD_USES_RECOVERY_CHARGEMODE := true
---
+++ shooter.mk
@@ -0,0 +1,176 @@
+#
+# Copyright (C) 2011 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# The gps config appropriate for this device
+PRODUCT_COPY_FILES += \
+    device/htc/shooter/gps.conf:system/etc/gps.conf
+
+## (1) First, the most specific values, i.e. the aspects that are specific to GSM
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.clientidbase=android-htc \
+    ro.com.google.locationfeatures=1 \
+    ro.com.google.networklocation=1 \
+    ro.setupwizard.enable_bypass=1 \
+    dalvik.vm.lockprof.threshold=500 \
+    dalvik.vm.dexopt-flags=m=y
+
+PRODUCT_COPY_FILES += \
+    device/htc/shooter/init.shooter.rc:root/init.shooter.rc \
+    device/htc/shooter/ueventd.shooter.rc:root/ueventd.shooter.rc
+
+## (2) Also get non-open-source GSM-specific aspects if available
+$(call inherit-product-if-exists, vendor/htc/shooter/shooter-vendor.mk)
+
+## (3)  Finally, the least specific parts, i.e. the non-GSM-specific aspects
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.com.google.clientidbase=android-sprint-us \
+	ro.com.google.locationfeatures=1 \
+	ro.cdma.home.operator.numeric=310120 \
+	ro.cdma.home.operator.alpha=Sprint \
+	ro.setupwizard.enable_bypass=1 \
+	ro.media.dec.jpeg.memcap=20000000 \
+	dalvik.vm.lockprof.threshold=500 \
+	dalvik.vm.dexopt-flags=m=y \
+	ro.opengles.version=131072
+
+DEVICE_PACKAGE_OVERLAYS += device/htc/shooter/overlay
+
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
+    frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/base/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
+    frameworks/base/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
+
+#PRODUCT_PACKAGES += \
+#    gps.shooter \
+#    librs_jni \
+#    gralloc.msm8660 \
+#    copybit.msm8660 \
+#    overlay.default \
+#    com.android.future.usb.accessory
+
+PRODUCT_PACKAGES += \
+    librs_jni \
+#    libOmxCore \
+    libOmxVenc \
+    libOmxVdec \
+#    libaudio \
+    com.android.future.usb.accessory
+
+# Keylayouts
+PRODUCT_COPY_FILES += \
+    device/htc/shooter/keychars/qwerty2.kcm.bin:system/usr/keychars/qwerty2.kcm.bin \
+    device/htc/shooter/keychars/qwerty.kcm.bin:system/usr/keychars/qwerty.kcm.bin \
+    device/htc/shooter/keychars/shooter-keypad.kcm.bin:system/usr/keychars/shooter-keypad.kcm.bin \
+    device/htc/shooter/keychars/BT_HID.kcm.bin:system/usr/keychars/BT_HID.kcm.bin \
+    device/htc/shooter/keylayout/h2w_headset.kl:system/usr/keylayout/h2w_headset.kl \
+    device/htc/shooter/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl \
+    device/htc/shooter/keylayout/shooter-keypad.kl:system/usr/keylayout/shooter-keypad.kl \
+    device/htc/shooter/keylayout/BT_HID.kl:system/usr/keylayout/BT_HID.kl \
+    device/htc/shooter/keylayout/AVRCP.kl:system/usr/keylayout/AVRCP.kl
+# Firmware
+PRODUCT_COPY_FILES += \
+    device/htc/shooter/firmware/BCM4329B1_002.002.023.0589.0632.hcd:system/etc/firmware/BCM4329B1_002.002.023.0589.0632.hcd \
+    device/htc/shooter/firmware/fw_bcm4329.bin:system/etc/firmware/fw_bcm4329.bin \
+    device/htc/shooter/firmware/fw_bcm4329_apsta.bin:system/etc/firmware/fw_bcm4329_apsta.bin \
+    device/htc/shooter/firmware/vidc_1080p.fw:system/etc/firmware/vidc_1080p.fw \
+    device/htc/shooter/firmware/leia_pfp_470.fw:system/etc/firmware/leia_pfp_470.fw \
+    device/htc/shooter/firmware/leia_pm4_470.fw:system/etc/firmware/leia_pm4_470.fw
+    
+# Audio DSP Profiles
+PRODUCT_COPY_FILES += \
+    device/htc/shooter/dsp/AIC3254_REG.csv:system/etc/AIC3254_REG.csv \
+    device/htc/shooter/dsp/AIC3254_REG_DualMic.csv:system/etc/AIC3254_REG_DualMic.csv \
+    device/htc/shooter/dsp/AdieHWCodec.csv:system/etc/AdieHWCodec.csv \
+    device/htc/shooter/dsp/AudioBTID.csv:system/etc/AudioBTID.csv \
+    device/htc/shooter/dsp/CodecDSPID.txt:system/etc/CodecDSPID.txt \
+    device/htc/shooter/dsp/CodecDSPID_WB.txt:system/etc/CodecDSPID_WB.txt \
+    device/htc/shooter/dsp/TPA2051_CFG.csv:system/etc/TPA2051_CFG.csv \
+    device/htc/shooter/dsp/TPA2051_CFG_XC.csv:system/etc/TPA2051_CFG_XC.csv \
+    device/htc/shooter/dsp/soundimage/Sound_Original_Recording.txt:system/etc/soundimage/Sound_Original_Recording.txt \
+    device/htc/shooter/dsp/soundimage/Sound_Original_SPK.txt:system/etc/soundimage/Sound_Original_SPK.txt \
+    device/htc/shooter/dsp/soundimage/Sound_Original.txt:system/etc/soundimage/Sound_Original.txt \
+    device/htc/shooter/dsp/soundimage/Sound_Phone_Original_HP.txt:system/etc/soundimage/Sound_Phone_Original_HP.txt \
+    device/htc/shooter/dsp/soundimage/Sound_Phone_Original_REC.txt:system/etc/soundimage/Sound_Phone_Original_REC.txt \
+    device/htc/shooter/dsp/soundimage/Sound_Phone_Original_SPK.txt:system/etc/soundimage/Sound_Phone_Original_SPK.txt \
+    device/htc/shooter/dsp/soundimage/Sound_Rec_Landscape.txt:system/etc/soundimage/Sound_Rec_Landscape.txt \
+    device/htc/shooter/dsp/soundimage/Sound_Rec_Portrait.txt:system/etc/soundimage/Sound_Rec_Portrait.txt \
+    device/htc/shooter/dsp/soundimage/Sound_Recording.txt:system/etc/soundimage/Sound_Recording.txt \
+    device/htc/shooter/dsp/soundimage/srs_geq10.cfg:system/etc/soundimage/srs_geq10.cfg \
+    device/htc/shooter/dsp/soundimage/srsfx_trumedia_51.cfg:system/etc/soundimage/srsfx_trumedia_51.cfg \
+    device/htc/shooter/dsp/soundimage/srsfx_trumedia_movie.cfg:system/etc/soundimage/srsfx_trumedia_movie.cfg \
+    device/htc/shooter/dsp/soundimage/srsfx_trumedia_music.cfg:system/etc/soundimage/srsfx_trumedia_music.cfg \
+    device/htc/shooter/prebuilt/snd3254:system/bin/snd3254
+
+# Wifi Module
+PRODUCT_COPY_FILES += \
+    device/htc/shooter/modules/bcm4329.ko:system/lib/modules/bcm4329.ko \
+    device/htc/shooter/modules/sequans_sdio.ko:system/lib/modules/sequans_sdio.ko \
+    device/htc/shooter/modules/wimaxuart.ko:system/lib/modules/wimaxuart.ko \
+    device/htc/shooter/modules/spidev.ko:system/lib/modules/spidev.ko  \
+    device/htc/shooter/modules/lcd.ko:system/lib/modules/lcd.ko \
+    device/htc/shooter/modules/wimaxdbg.ko:system/lib/modules/wimaxdbg.ko 
+
+
+
+# we have enough storage space to hold precise GC data
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+# device uses high-density artwork where available
+PRODUCT_LOCALES += hdpi
+
+PRODUCT_COPY_FILES += \
+    device/htc/shooter/vold.fstab:system/etc/vold.fstab
+
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_eu_supl.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/htc/shooter/overlay
+
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+	LOCAL_KERNEL := device/htc/shooter/kernel
+else
+	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
+# media profiles and capabilities spec
+$(call inherit-product, device/htc/shooter/media_a1026.mk)
+
+# htc audio settings
+$(call inherit-product, device/htc/shooter/media_htcaudio.mk)
+
+# stuff common to all HTC phones
+$(call inherit-product, device/htc/common/common.mk)
+
+$(call inherit-product, build/target/product/full_base.mk)
+
+PRODUCT_NAME := full_shooter
+PRODUCT_DEVICE := shooter
+PRODUCT_MODEL := HTC Evo 3D
+PRODUCT_MANUFACTURER := HTC
