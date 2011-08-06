b267bccb7ad2d036bc7a862e99fa82222815339c
andrew.boren <andrew.boren@gmail.com>
2011-08-06 13:19:20 -0700

    Update shooter devoce config. Working on the extract-files.sh

--- shooter.mk
+++ shooter.mk
@@ -27,7 +27,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.lockprof.threshold=500 \
     dalvik.vm.dexopt-flags=m=y
 
+#TODO ---- What do we need to do to not use a static init.rc?
 PRODUCT_COPY_FILES += \
+    device/htc/shooter/init.rc:root/init.rc \
     device/htc/shooter/init.shooter.rc:root/init.shooter.rc \
     device/htc/shooter/ueventd.shooter.rc:root/ueventd.shooter.rc
 
@@ -128,9 +130,7 @@ PRODUCT_COPY_FILES += \
     device/htc/shooter/modules/wimaxuart.ko:system/lib/modules/wimaxuart.ko \
     device/htc/shooter/modules/spidev.ko:system/lib/modules/spidev.ko  \
     device/htc/shooter/modules/lcd.ko:system/lib/modules/lcd.ko \
-    device/htc/shooter/modules/wimaxdbg.ko:system/lib/modules/wimaxdbg.ko 
-
-
+    device/htc/shooter/modules/wimaxdbg.ko:system/lib/modules/wimaxdbg.ko
 
 # we have enough storage space to hold precise GC data
 PRODUCT_TAGS += dalvik.gc.type-precise
