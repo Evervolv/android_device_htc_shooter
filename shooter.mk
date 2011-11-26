ff8305d0d8883e10c552b80d5b99526594c81774
andrew.boren <andrew.boren@gmail.com>
2011-11-26 12:55:16 -0700

    Update kernel and modules for touchscreen.
    Change-Id: I195a6bcf1fec634ec9631e8afb64aa35269451ab

--- shooter.mk
+++ shooter.mk
@@ -130,10 +130,17 @@ PRODUCT_COPY_FILES += \
 
 # Wifi Module
 PRODUCT_COPY_FILES += \
-    device/htc/shooter/modules/bcm4329.ko:system/lib/modules/bcm4329.ko \
-    device/htc/shooter/modules/sequans_sdio.ko:system/lib/modules/sequans_sdio.ko \
-    device/htc/shooter/modules/wimaxuart.ko:system/lib/modules/wimaxuart.ko \
-    device/htc/shooter/modules/wimaxdbg.ko:system/lib/modules/wimaxdbg.ko
+    device/htc/supersonic/modules/bcm4329.ko:system/lib/modules/bcm4329.ko \
+    device/htc/supersonic/modules/sequans_sdio.ko:system/lib/modules/sequans_sdio.ko \
+    device/htc/supersonic/modules/auth_rpcgss.ko:system/lib/modules/auth_rpcgss.ko \
+    device/htc/supersonic/modules/cifs.ko:system/lib/modules/cifs.ko \
+    device/htc/supersonic/modules/lockd.ko:system/lib/modules/lockd.ko \
+    device/htc/supersonic/modules/nfs.ko:system/lib/modules/nfs.ko \
+    device/htc/supersonic/modules/rpcsec_gss_krb5.ko:system/lib/modules/rpcsec_gss_krb5.ko \
+    device/htc/supersonic/modules/sunrpc.ko:system/lib/modules/sunrpc.ko \
+    device/htc/supersonic/modules/tun.ko:system/lib/modules/tun.ko \
+    device/htc/supersonic/modules/wimaxdbg.ko:system/lib/modules/wimaxdbg.ko \
+    device/htc/supersonic/modules/wimaxuart.ko:system/lib/modules/wimaxuart.ko
 
 # we have enough storage space to hold precise GC data
 PRODUCT_TAGS += dalvik.gc.type-precise
