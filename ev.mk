34785aa79a6b73c725c60c49e014f58ef2995035
andrew.boren <andrew.boren@gmail.com>
2011-12-22 21:45:55 -0700

    Update some more!
    Change-Id: I72237f75caf846037c56012037e4250048e24eb5

--- ev.mk
+++ ev.mk
@@ -12,19 +12,11 @@ PRODUCT_BRAND := sprint
 PRODUCT_DEVICE := shooter
 PRODUCT_MODEL := PG8610000
 PRODUCT_MANUFACTURER := HTC
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_shooter BUILD_ID=GRI40 BUILD_DISPLAY_ID=GRJ90 BUILD_FINGERPRINT=sprint/htc_shooter/shooter:2.3.3/GRI40/83920:user/release-keys PRIVATE_BUILD_DESC="1.13.651.7 CL83920 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_shooter BUILD_ID=GRI40 BUILD_FINGERPRINT=sprint/htc_shooter/shooter:2.3.3/GRI40/83920:user/release-keys PRIVATE_BUILD_DESC="1.13.651.7 CL83920 release-keys"
 
 # Set up the product codename, build version & MOTD.
 PRODUCT_CODENAME := Neco
-
-ifeq ($(NIGHTLY_BUILD),true)
-    BUILD_VERSION := 2.0.0p1$(shell date +%m%d%Y)-NIGHTLY
-else
-    BUILD_VERSION := 2.0.0p1
-endif
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.romversion=Evervolv-$(PRODUCT_CODENAME)-$(BUILD_VERSION)
+PRODUCT_VERSION_DEVICE_SPECIFIC := p1
 
 PRODUCT_MOTD :="\n\n\n--------------------MESSAGE---------------------\nThank you for choosing Evervolv for your Droid Incredible 2\nPlease visit us at \#evervolv on irc.freenode.net\nFollow @preludedrew for the latest Evervolv updates\nGet the latest rom at evervolv.com\n------------------------------------------------\n"
 
