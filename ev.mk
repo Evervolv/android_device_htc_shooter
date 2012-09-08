e1b0e9750976b113d4205b414da630d2b8a5379d
andrew.boren <andrew.boren@gmail.com>
2012-09-08 16:37:13 -0700

    shooter: Fix syntax error, missing end quote.
    Change-Id: Id38e16023d771af40269ad2eb0f5e22414a71670

--- ev.mk
+++ ev.mk
@@ -12,7 +12,7 @@ PRODUCT_BRAND := sprint
 PRODUCT_DEVICE := shooter
 PRODUCT_MODEL := PG86100
 PRODUCT_MANUFACTURER := HTC
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_shooter BUILD_FINGERPRINT="sprint/htc_shooter/shooter:4.0.3/IML74K/130920.2:user/release-keys" PRIVATE_BUILD_DESC="2.08.651.2 CL130920 release-keys
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_shooter BUILD_FINGERPRINT="sprint/htc_shooter/shooter:4.0.3/IML74K/130920.2:user/release-keys" PRIVATE_BUILD_DESC="2.08.651.2 CL130920 release-keys"
 
 # Set up the product codename, build version & MOTD.
 PRODUCT_CODENAME := Neco
